7bc33a0defd86a9108ccd263b8caf6f94fba747f
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2020-12-11 20:48:23 +0000

    Clean up day 11

--- AOC2020/Day11.cs
+++ AOC2020/Day11.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Collections.Generic;
+﻿using System.Linq;
 
 namespace AOC2020
 {
@@ -15,119 +12,68 @@ namespace AOC2020
             seats = ir.GetInput('\n').Select(line => line.ToCharArray()).ToArray();
         }
 
-        public override long RunSilver()
-        {
-            return GetOccupiedSeats(true, 4);
-        }
+        public override long RunSilver() => GetOccupiedSeats(true, 4);
 
-        public override long RunGold()
-        {
-            return GetOccupiedSeats(false, 5);
-        }
+        public override long RunGold() => GetOccupiedSeats(false, 5);
 
         private int GetOccupiedSeats(bool checkFirst, int tolerance)
         {
-            // Uncomment code for visualisation
-            //int j = 0;
-            copy = new char[seats.Length][]; // Store the initial state for part 2
-            for (int i = 0; i < seats.Length; i++)
-            {
-                copy[i] = new char[seats[i].Length];
-                Array.Copy(seats[i], copy[i], copy[i].Length);
-            }
+            copy = seats; // Don't change the initial state
             while (true)
             {
                 char[][] temp = ApplyRules(checkFirst, tolerance);
                 bool unchanged = true;
                 for (int i = 0; i < temp.Length; i++)
                 {
-                    if (!temp[i].SequenceEqual(seats[i]))
+                    if (!temp[i].SequenceEqual(copy[i]))
                     {
                         unchanged = false;
                         break;
                     }
                 }
-                if (unchanged)
-                {
-                    seats = copy; // Reset initial state
-                    return temp.Select(r => r.Count(c => c == '#')).Sum();
-                }
-                seats = temp;
-
-                /*if ((j++) % 2 == 0)
-                {
-                    Visualise();
-                    Thread.Sleep(75);
-                }*/
+                if (unchanged) return temp.Select(r => r.Count(c => c == '#')).Sum();
+                copy = temp;
             }
         }
 
         private char[][] ApplyRules(bool onlyAdj, int tolerance)
         {
-            char[][] newSeats = new char[seats.Length][];
-            for (int y = 0; y < seats.Length; y++)
+            char[][] newSeats = new char[copy.Length][];
+            for (int y = 0; y < copy.Length; y++)
             {
-                char[] row = new char[seats[0].Length];
-                for (int x = 0; x < seats[0].Length; x++)
+                newSeats[y] = new char[copy[0].Length];
+                for (int x = 0; x < copy[0].Length; x++)
                 {
                     int adj = 0;
                     for (int dy = -1; dy <= 1; dy++)
                     {
-                        if (y + dy < 0 || y + dy >= seats.Length)
-                            continue;
+                        if (y + dy < 0 || y + dy >= copy.Length) continue;
                         for (int dx = -1; dx <= 1; dx++)
                         {
-                            if ((dx == 0 && dy == 0) || x + dx < 0 || x + dx >= seats[0].Length)
-                                continue;
+                            if ((dx == 0 && dy == 0) || x + dx < 0 || x + dx >= copy[0].Length) continue;
                             adj += OccupiedSeatInDir(onlyAdj, y, x, dy, dx) ? 1 : 0;
                         }
                     }
-                    if (seats[y][x] == '.') row[x] = '.';
-                    else if (seats[y][x] == 'L') row[x] = adj == 0 ? '#' : 'L';
-                    else if (seats[y][x] == '#') row[x] = adj >= tolerance ? 'L' : '#';
+                    if (copy[y][x] == '.') newSeats[y][x] = '.';
+                    else if (copy[y][x] == 'L') newSeats[y][x] = adj == 0 ? '#' : 'L';
+                    else if (copy[y][x] == '#') newSeats[y][x] = adj >= tolerance ? 'L' : '#';
                 }
-                newSeats[y] = row;
             }
             return newSeats;
         }
 
         private bool OccupiedSeatInDir(bool onlyAdj, int y, int x, int dy, int dx)
         {
-            if (onlyAdj)
-                return seats[y + dy][x + dx] == '#';
+            if (onlyAdj) return copy[y + dy][x + dx] == '#';
 
             for(int tx = x + dx, ty = y + dy;
-                tx >= 0 && tx < seats[0].Length && ty >= 0 && ty < seats.Length;
+                tx >= 0 && tx < copy[0].Length && ty >= 0 && ty < copy.Length;
                 tx += dx, ty += dy)
             {
-                if (seats[ty][tx] == 'L')
-                    return false;
-                if (seats[ty][tx] == '#')
-                    return true;
+                if (copy[ty][tx] != '.')
+                    return copy[ty][tx] == '#';
             }
             return false;
-        }
-
-        Dictionary<char, ConsoleColor> colours = new Dictionary<char, ConsoleColor>()
-        {
-            { '.', ConsoleColor.Black },
-            { 'L', ConsoleColor.Red },
-            { '#', ConsoleColor.Green }
-        };
-
-        public void Visualise()
-        {
-            Console.SetCursorPosition(0, 0);
-            foreach(char[] row in seats)
-            {
-                foreach(char c in row)
-                {
-                    Console.BackgroundColor = colours[c];
-                    Console.Write("  ");
-                }
-                Console.SetCursorPosition(0, Console.CursorTop + 1);
-            }
-            Console.ResetColor();
         }
     }
 }
