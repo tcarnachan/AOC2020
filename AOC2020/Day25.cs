770a2e3c5c1213bf196ae99423f083c3294e5de8
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2020-12-25 12:35:42 +0000

    Christmas!

---
+++ AOC2020/Day25.cs
@@ -0,0 +1,25 @@
+﻿namespace AOC2020
+{
+    public class Day25
+    {
+        int[] keys;
+        int subjNo = 7;
+
+        public Day25()
+        {
+            InputReader ir = new InputReader(25);
+            keys = ir.GetIntArray();
+        }
+
+        public long RunSilver()
+        {
+            long loop, t = 1;
+            for (loop = 0; t != keys[0]; loop++) t = (t * subjNo) % 20201227;
+            (subjNo, t) = (keys[1], 1);
+            for (int i = 0; i < loop; i++) t = (t * subjNo) % 20201227;
+            return t;
+        }
+
+        public string RunGold() => "No puzzle";
+    }
+}
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -10,7 +10,7 @@ namespace AOC2020
     {
         public static void Main()
         {
-            var solution = new Day24();
+            var solution = new Day25();
 
             Stopwatch sw = Stopwatch.StartNew();
             var res = solution.RunSilver();
